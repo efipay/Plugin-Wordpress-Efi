a06259d947070d31f8f242988eff1ed2ec8b1e38
Joao Oliveira <joao.oliveira@gerencianet.com.br>
2021-03-05 16:32:21 -0300

    correção pix copia e cola, botão adicionado

--- manual/woo-gerencianet-official/templates/order-received.php
+++ manual/woo-gerencianet-official/templates/order-received.php
@@ -9,15 +9,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.6/clipboard.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+
 <div>
     <div class="gn-success-payment">
         <div class="gn-row gn-box-emission">
             <div class="pull-left gn-left-space-2">
                 <img src="<?php echo esc_url( plugins_url( 'assets/images/', plugin_dir_path( __FILE__ ) ) ); ?>gerencianet-configurations.png"
-                     alt="Gerencianet" title="Gerencianet"/>
+                    alt="Gerencianet" title="Gerencianet" />
             </div>
             <div class="pull-left gn-title-emission">
-				<?php echo $showText[$generated_payment_type]['title']; ?>
+                <?php echo $showText[$generated_payment_type]['title']; ?>
             </div>
             <div class="clear"></div>
         </div>
@@ -30,13 +34,13 @@ if ( ! defined( 'ABSPATH' ) ) {
                     </div>
                 </div>
 
-                <div class="gn-col-10 gn-success-payment-billet-comments">
-					<?php echo $showText[$generated_payment_type]['content']; ?>
+                <div class="gn-col-10 gn-success-payment-billet-comments" style="font-size: 15px!important;">
+                    <?php echo $showText[$generated_payment_type]['content']; ?>
 
                     <?php if ($charge_id != '') { ?>
-                        <p>
-    						<?php echo $gn_success_payment_charge_number; ?> <b><?php echo $charge_id; ?></b>
-                        </p>
+                    <p>
+                        <?php echo $gn_success_payment_charge_number; ?> <b><?php echo $charge_id; ?></b>
+                    </p>
                     <?php } ?>
                 </div>
 
@@ -45,30 +49,53 @@ if ( ! defined( 'ABSPATH' ) ) {
             <?php if ($qrcode !== "") { ?>
             <div class="gn-qrcode" style="text-align:center;">
                 <img src="<?php echo $qrcode; ?>" />
-                <div style="font-size: 15px!important;text-align: center !important;">
 
-                    <span style="font-weight: bold !important;word-break: break-all;">
-                        <?php echo __('Or copy the Code below and paste it into the app where you are going to make the payment:', WCGerencianetOficial::getTextDomain()) ; ?>
-                    </span>
-                    <span style=" max-width: 400px!important; display: inline-block !important">
-                        <?php echo $pixCopiaCola;  ?>
-                    </span>
+                <div class="gn-row" style="font-size: 15px!important;text-align: center !important;">
+                    <div class="gn-col-12">
+                        <span style="font-weight: bold !important;">
+                            <?php echo __('Or copy the Code below and paste it into the app where you are going to make the payment:', WCGerencianetOficial::getTextDomain()) ; ?>
+                        </span>
+                    </div>
+                    <script>
+                    var clipboard = new ClipboardJS('#btnCopiar');
+                    clipboard.on('success', function(e) {
+                        Swal.fire(
+                            'Código Pix copiado com sucesso!',
+                            '',
+                            'success'
+                        );
+                        e.clearSelection();
+                    });
+                    </script>
+                    <div class="gn-row">
+                        <div class="gn-col-12">
+                            <textarea name="pixCopiaCola" style="width: 500px;margin-left: auto;margin-right: auto;"
+                                id="pixCopiaCola" cols="30" rows="10" disabled><?php echo $pixCopiaCola;  ?></textarea>
+                        </div>
+                    </div>
+                    <div class="gn-col-12">
+                        <button id="btnCopiar" data-clipboard-text="<?php echo $pixCopiaCola;  ?>">Copiar Código
+                            Pix!</button>
+                    </div>
+
+
 
                 </div>
+
             </div>
             <?php } ?>
 
-			<?php if ( $generated_payment_type == "billet" && $billet_url != "" ) { ?>
-                <div class="gn-align-center gn-success-payment-button">
-                    <button class="button" id="showBillet" name="showBillet"
-                            onclick="window.open('<?php echo $billet_url; ?>', '_blank');" style="height: auto;">
-                        <div class="gn-success-payment-button-icon pull-left"><span class="gn-icon-download"></span>
-                        </div>
-                        <div class="pull-left"><?php echo $gn_success_payment_open_billet; ?></div>
-                        <div class="clear"></div>
-                    </button>
-                </div>
-			<?php } ?>
+            <?php if ( $generated_payment_type == "billet" && $billet_url != "" ) { ?>
+            <div class="gn-align-center gn-success-payment-button">
+                <button class="button" id="showBillet" name="showBillet"
+                    onclick="window.open('<?php echo $billet_url; ?>', '_blank');" style="height: auto;">
+                    <div class="gn-success-payment-button-icon pull-left"><span class="gn-icon-download"></span>
+                    </div>
+                    <div class="pull-left"><?php echo $gn_success_payment_open_billet; ?></div>
+                    <div class="clear"></div>
+                </button>
+            </div>
+            <?php } ?>
         </div>
     </div>
 </div>
