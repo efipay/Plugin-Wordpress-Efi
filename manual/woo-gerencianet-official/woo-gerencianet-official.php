7b59b84533683f630883dff90d31412225db0883
Joao Oliveira <joao.oliveira@gerencianet.com.br>
2021-07-26 14:02:49 -0300

    Nova release: v1.3.6

--- manual/woo-gerencianet-official/woo-gerencianet-official.php
+++ manual/woo-gerencianet-official/woo-gerencianet-official.php
@@ -5,7 +5,7 @@
  * Description: Gateway de pagamento Gerencianet para WooCommerce.
  * Author: Gerencianet
  * Author URI: http://www.gerencianet.com.br
- * Version: 1.3.5
+ * Version: 1.3.6
  * License: GPLv2 or later
  * Text Domain: woo-gerencianet-official
  * Domain Path: /languages
@@ -28,7 +28,7 @@ if (!class_exists('WCGerencianetOficial')) :
 		 *
 		 * @var string
 		 */
-		const VERSION = '1.3.5';
+		const VERSION = '1.3.6';
 
 		/**
 		 * Integration id.
