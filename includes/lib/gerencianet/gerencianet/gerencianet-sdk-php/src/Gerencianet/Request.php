869ea87c4be41672a94daea609ae9c725b2d0b8f
Palloma Brito <palloma.brito@sejaefi.com.br>
2023-05-24 10:14:22 -0300

    Atualização de versão

--- includes/lib/gerencianet/gerencianet/gerencianet-sdk-php/src/Gerencianet/Request.php
+++ includes/lib/gerencianet/gerencianet/gerencianet-sdk-php/src/Gerencianet/Request.php
@@ -27,7 +27,7 @@ class Request
             'headers' => [
                 'Content-Type' => 'application/json',
                 'api-sdk' => 'php-' . $composerData['version'],
-                'wordpress-plugin' => 'v2.0.11',  // ATUALIZAR VERSÃO A CADA ATUALIZAÇÃO
+                'wordpress-plugin' => 'v2.0.12',  // ATUALIZAR VERSÃO A CADA ATUALIZAÇÃO
                 'partner-token' => $partner_token
             ]
         ];
